13120bcb614ce184a7702fbd238e6c06fb32e814
jordan <jordansilva102@gmail.com>
2022-02-26 22:00:20 -0300

    basic styled-components configuration

--- frontend/my-portfolio/src/App.js
+++ frontend/my-portfolio/src/App.js
@@ -1,8 +1,30 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+	*{
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+		background-color: #12121e;
+	}
+`;
+
+const MainWrapper = styled.div``;
+
+const Title = styled.h1`
+	font-size: 1.5em;
+	text-align: center;
+	color: #ce86e7;
+	font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+		"Lucida Sans", Arial, sans-serif;
+`;
+
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'></header>
-		</div>
+		<MainWrapper>
+			<GlobalStyle />
+			<Title>Hello World!</Title>
+		</MainWrapper>
 	);
 }
 
